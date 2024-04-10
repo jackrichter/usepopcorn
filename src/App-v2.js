b32368b479d121b4c9ce41963fbc3043cf365050
jackrichter <jackrichter1@gmail.com>
2024-04-10 22:28:20 +0200

    Changing the page title to reflect a selected movie.

--- src/App-v2.js
+++ src/App-v2.js
@@ -345,6 +345,11 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     [selectedId]
   );
 
+  useEffect(() => {
+    if (!title) return;
+    document.title = `Movie | ${title}`;
+  }, [title]);
+
   return (
     <div className="details">
       {isLoading ? (
