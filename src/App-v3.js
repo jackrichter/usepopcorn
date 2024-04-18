009070142ede6e433334f783943cf4a3b029fd79
jackrichter <jackrichter1@gmail.com>
2024-04-18 15:06:59 +0200

    Removed useMovies.

--- src/App-v3.js
+++ src/App-v3.js
@@ -1,6 +1,5 @@
 import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
-import { useMovies } from "./useMovies";
 
 const average = arr => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
@@ -8,11 +7,13 @@ const KEY = "4d7a12b1";
 
 export default function App() {
   // An example of 'Prop Drilling'
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
   const [query, setQuery] = useState("");
   const [selectedId, setSelectedId] = useState(null);
 
-  // Custom Hook useMovies
-  const { movies, isLoading, error } = useMovies(query, handleCloseMovie);
+  // const { movies, isLoading, error } = useMovies(query, handleCloseMovie);
 
   // Read back from local storage using lazy initial state
   // const [watched, setWatched] = useState([]);
@@ -42,6 +43,54 @@ export default function App() {
     setWatched(movie => watched.filter(movie => movie.imdbID !== id));
   }
 
+  useEffect(
+    function () {
+      const controller = new AbortController();
+
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
+          setError("");
+          const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`, {
+            signal: controller.signal
+          });
+
+          if (!res.ok) throw new Error("Something went wrong with fetching movies");
+
+          const data = await res.json();
+
+          if (data.Response === "False") throw new Error("Movie not found");
+
+          setMovies(data.Search);
+          setError("");
+        } catch (err) {
+          console.log(err.message);
+
+          if (error.name !== "AbortError") {
+            setError(err.message);
+          }
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+
+      handleCloseMovie();
+      fetchMovies();
+
+      // Cleanup function
+      return function () {
+        controller.abort();
+      };
+    },
+    [query, error.name]
+  );
+
   // Save the watched list (array) to browser local storage. BUT now using an Effect!
   useEffect(
     function () {
@@ -66,11 +115,7 @@ export default function App() {
           ) : isLoading ? (
             <Loader />
           ) : (
-            <MovieList
-              movies={movies}
-              onSelectMovie={handleSelectMovie}
-              onCloseMovie={handleCloseMovie}
-            />
+            <MovieList movies={movies} onSelectMovie={handleSelectMovie} />
           )}
         </Box>
         <Box>
